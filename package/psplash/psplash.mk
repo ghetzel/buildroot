6fe16c62a63df1320a1648a379e8896626339482
Gary Hetzel <garyhetzel@gmail.com>
2014-04-25 17:06:27 -0400

    Initial catalina build

--- package/psplash/psplash.mk
+++ package/psplash/psplash.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PSPLASH_VERSION = afd4e228c606a9998feae44a3fed4474803240b7
-PSPLASH_SITE = git://git.yoctoproject.org/psplash
+PSPLASH_VERSION = 923686d27061d496b70514a4eb645e2c6a31fa8e
+PSPLASH_SITE_METHOD = git
+PSPLASH_SITE = https://github.com/ghetzel/psplash.git
 PSPLASH_LICENSE = GPLv2+
 PSPLASH_AUTORECONF = YES
 
